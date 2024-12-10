17a22cad0c48dc43a85d0c03f486138e3c983bcd
Andrey Valiuk <valyuk.andrey.04@mail.ru>
2024-12-10 23:30:57 +0300

    commit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,7 @@
 export const App = () => {
     return (
         <div>
-            <header className="header">
-                <div className="container">
-                    <div className="logo">
-                        Logo
-                    </div>
-
-                    <div className="burger">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-                </div>
-            </header>
+           
         </div>
     )
 }
